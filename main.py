01b46a2d551a8a68e73d5c132f5a4d75eeb30e04
VicAnto99 <A01339529@itesm.mx>
2020-05-16 14:03:20 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#Imports
+#import tkinter
+from tkinter import *
+
+#Definitions
+def read():
+    t = txt.get()
+    text.set(t)
+#Main
+text = StringVar()
+
+window = Tk()
+window.title("Integrative practice 2")
+window.maxsize(1010, 610)
+window.config(bg = "skyblue")
+
+#Left_Frame_design
+left_frame = Frame(window, width = 300, height = 590, bg='grey')
+left_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+in_1 = Label(left_frame, text = "INSTRUCTIONS").grid(row = 0, column = 0, padx = 5, pady = 5)
+tool_bar = Frame(left_frame, width = 280, height = 550)
+tool_bar.grid(row = 2, column = 0, padx = 5, pady = 5)
+in_2 = Label(tool_bar, text = "Insert in the next text box the name of your txt \n Example: test1.txt").grid(row = 0, column = 0, padx = 5, pady = 5)
+txt = Entry(tool_bar, width = 46).grid(row = 2, column = 0, padx = 5, pady = 5)
+but_1 = Button(tool_bar, text='Read', command = read).grid(row=3)
+in_3 = Label(tool_bar, textvariable = text).grid(row = 3, column = 1)
+
+#Right_Frame_design
+right_frame = Frame(window, width = 670, height = 590, bg='grey')
+right_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+
+window.mainloop()
