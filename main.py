15b4047a88cc8b4cf6629fac3f3d9eed652a92a6
VicAnto99 <A01339529@itesm.mx>
2020-05-16 14:38:08 -0500

    Prueba Victor

--- main.py
+++ main.py
@@ -31,5 +31,5 @@ in_3 = Label(tool_bar, textvariable = txt).grid(row = 3, column = 1)
 right_frame = Frame(window, width = 670, height = 590, bg='grey')
 right_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
 
-
+#Prueba vic
 window.mainloop()
